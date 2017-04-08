9c4d0d38a4adff6698c96a0aa6bda9e4cd3b4fc5
NadimRai <rai_nadim@yahoo.com>
2017-04-07 23:14:11 -0700

    added jumbotron

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent  {
 
-  constructor() { }
+  branding = " Simple Angular Site";
+  private jbtHeading:string;
+  private jbtText:string;
+  private jbtBtnText:string;
+  private jbtBtnUrl:string;
 
-  ngOnInit() {
+  constructor(){
+  	this.jbtHeading="Hello World";
+  	this.jbtText = "This is a simpe hero unit";
+  	this.jbtBtnText = "Read More";
+  	this.jbtBtnUrl = "/about";
   }
+
 
 }
