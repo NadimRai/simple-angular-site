1af3acfad44d749492a14fd39b7c3d47fa80373f
NadimRai <rai_nadim@yahoo.com>
2017-04-09 21:28:16 -0700

    example of click event and binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
+	allowNewServer = false;
+	serverCreationStatus = 'No server was created!';
 	serverName = "TestServer";
-  constructor() { }
+  constructor() { 
+  	setTimeout(() => {
+  		this.allowNewServer =  true;
+  	}, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer(){
+  	this.serverCreationStatus = 'Server was created!';
+  }
+
+  
 }
